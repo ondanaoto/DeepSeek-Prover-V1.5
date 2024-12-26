0fb9396957dd6e72d763d7638b3f902793a5b8a0
ondanaoto <n.onda.go20@gmail.com>
2024-12-26 02:39:44 +0000

    fetch nontrivial conjcture

--- repository/conjecture_repository.py
+++ repository/conjecture_repository.py
@@ -1,15 +1,13 @@
 import json
 
-def fetch_all() -> list[str]:
-    with open('/data/conjectures.jsonl', 'r') as f:
+def fetch_conjectures(
+        nontrivial_only: bool = False,
+) -> list[str]:
+    with open('/data/eval_conjecture_result.jsonl', 'r') as f:
         conjectures = [json.loads(line) for line in f]
         
-    conjectures = [c['conjecture'] for c in conjectures]
-    return conjectures
-
-def fetch_by_datetime(datetime: str) -> list[str]:
-    with open('/data/conjectures.jsonl', 'r') as f:
-        conjectures = [json.loads(line) for line in f]
-        
-    conjectures = [c['conjecture'] for c in conjectures if c['experiment_date_time'] == datetime]
-    return conjectures
+    conjectures = [c for c in conjectures if c["grammatical"]]
+    if nontrivial_only:
+        conjectures = [c for c in conjectures if not (c["already_exists"] or c["aesop_provable"]) ]
+    
+    return [c["conjecture"] for c in conjectures]
